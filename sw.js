a53fe838f756dee1b500e18ef4bad0811444282d
jcorporation <mail@jcgames.de>
2023-05-08 23:21:43 +0200

    ServiceWorker Cache Ausnahmen hinzugefügt

--- sw.js
+++ sw.js
@@ -4,13 +4,16 @@ const cacheName = "{{ "now" | date: "%Y-%m-%d_%H-%M" }}";
 const contentToCache = [
     {%- assign pages = site.pages %}
     {%- for page in pages %}
-        {%- if page.url == '/sw.js' %}
-            {%- continue %}
-        {%- endif %}
+        {%- if page.url == '/sw.js' %}{% continue %}{% endif %}
+        {%- if page.url == '/feed.xml' %}{% continue %}{% endif %}
+        {%- if page.url == '/404.html' %}{% continue %}{% endif %}
         "{{page.url}}",
     {%- endfor %}
     {%- assign pages = site.pages %}
     {%- for file in site.static_files %}
+        {%- if file.path == '/README.md' %}{% continue %}{% endif %}
+        {%- if file.path == '/CNAME' %}{% continue %}{% endif %}
+        {%- if file.path == '/spacepirates.webmanifest' %}{% continue %}{% endif %}
         "{{file.path}}"
         {%- unless forloop.last %},{%- endunless %}
     {%- endfor %}
